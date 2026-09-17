b4d826399bf79b301c838f025d0705ec76b020ea
Ashish kumar <157211553+Ashish200545@users.noreply.github.com>
2026-09-17 17:20:01 +0530

    Add files via upload

---
+++ N_Queen.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class N_Queen {
+
+    static boolean isSafe(char[][] board, int row, int col, int n) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void solve(char[][] board, int row, int n) {
+        if (row == n) {
+            printBoard(board, n);
+            System.out.println();
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+
+            if (isSafe(board, row, col, n)) {
+                board[row][col] = 'Q';
+                solve(board, row + 1, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    static void printBoard(char[][] board, int n) {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+        solve(board, 0, n);
+        sc.close();
+    }
+}
